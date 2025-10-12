9d586eb0164e28498f13ef56242aedd52918c180
MadCreeper7611 <171872796+madcreeper7611@users.noreply.github.com>
2025-10-11 23:23:53 -0400

    Added version 0.0.0.1 of MAWS

---
+++ MAWS/AgentForm.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Net
+
+Public Class AgentForm
+    Public ScriptURL As String = Command()
+    Dim client As New WebClient
+
+    Public Sub New()
+        InitializeComponent()
+        Hide()
+    End Sub
+
+    Private Sub AgentForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        If ScriptURL = Nothing Then
+            LoadFileDialog.Show()
+        Else
+            ReadScript()
+        End If
+    End Sub
+
+    Public Sub ReadScript()
+        Try
+            Dim ScriptText = client.DownloadString(ScriptURL)
+        Catch ex As Exception
+            MessageBox.Show("There was an error while loading the script:" & ex.Message)
+        End Try
+    End Sub
+End Class
