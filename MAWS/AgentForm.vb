71dce9efe47d4fab0afd58953fd5b7a87d184900
MadCreeper7611 <171872796+madcreeper7611@users.noreply.github.com>
2025-10-30 21:48:58 -0400

    Added Version 1.1.0 files

--- MAWS/AgentForm.vb
+++ MAWS/AgentForm.vb
@@ -10,8 +10,12 @@ Public Class AgentForm
     Dim CharIDs As New List(Of String)
     ' Hide Requests to make sure each agent is hidden before the program closes.
     Dim HideReq As IAgentCtlRequest
+    ' The amount of commands that each character has.
+    Dim CommandsCount As Integer
     ' The client needed for downloading the data in the script.
     Dim client As New WebClient
+    ' The text contained in the script.
+    Dim ScriptText As String
     ' Variable for making requests
     Dim Req
 
@@ -30,7 +34,7 @@ Public Class AgentForm
         End If
     End Sub
 
-    ' Extremely useful to prevent agents from speaking at the same time and giving things time to load.
+    ' Extremely useful to prevent agents from speaking/thinking/playing actions at the same time and giving things time to load.
     Private Sub WaitFor(ByVal Request)
         On Error Resume Next
         For Each Agent As IAgentCtlCharacter In ControlAxAgent.Characters
@@ -43,22 +47,26 @@ Public Class AgentForm
         ' For example: If the current parse is [Characters] then it will start adding Characters to the group.
         Dim CurrentParse As String
 
+        ' Removes quotation marks in the command line arguments.
         ScriptURL = ScriptURL.Replace("""", "")
 
         If ScriptURL.StartsWith("msagentweb://") Then
+            ' Replaces the start of the script with http:// if it starts with msagentweb://.
             ScriptURL = ScriptURL.Replace("msagentweb://", "http://")
         ElseIf Not ScriptURL.StartsWith("http://") AndAlso Not ScriptURL.StartsWith("https://") Then
+            ' Adds http:// to the start of the script if there's no protocol infront of the URL.
             ScriptURL = "http://" & ScriptURL
         End If
 
         Try
-            Dim ScriptText = client.DownloadString(ScriptURL)
+            ' Downloads the data from the URL of the MSH script.
+            ScriptText = client.DownloadString(ScriptURL)
 
             For Each RL In ScriptText.Split(Chr(10))
+                ' Removes unnecessary characters from the line like spaces that come after the line.
                 Dim Line = RL.Trim
                 If Not Line = String.Empty Then
-                    ' Removes unnecessary characters such as spaces and such.
-                    ' Checks if the line is a list, if it is, set it as the current parse.
+                    ' Checks if the line is a list declaration, if it is, set it as the current parse.
                     If RL.StartsWith("[") Then
                         CurrentParse = Line
                     Else
@@ -85,40 +93,65 @@ Public Class AgentForm
                                 ' Causes a specific character to do an action from the line.
                                 GetActionFromLine(Line)
                             End If
+                        ElseIf CurrentParse = "[Commands]" Then
+                            For Each CharID In CharIDs
+                                If Line.ToLower.StartsWith("menuname") Then
+                                    ' Sets the caption of the menu to the value the script declares it as.
+                                    ControlAxAgent.Characters(CharID).Commands.Caption = AfterEquals
+                                Else
+                                    ' Adds each character the commands declared in the script and gets the amount of commands.
+                                    ControlAxAgent.Characters(CharID).Commands.Add(Line.Remove(Line.LastIndexOf("=")), AfterEquals.Remove(AfterEquals.IndexOf("|")), AfterEquals.Substring(AfterEquals.LastIndexOf("|") + 1))
+                                    CommandsCount = ControlAxAgent.Characters(CharID).Commands.Count
+                                End If
+                            Next
                         End If
                     End If
-
                 End If
             Next
 
-            HideAllCharacters()
+            ' Hides every character in a script if the script has less than 2 commands.
+            If CommandsCount < 2 Then
+                HideAllCharacters()
+            End If
         Catch ex As Exception
             MessageBox.Show("There was an error while loading the script:" & ex.Message)
             Close()
         End Try
     End Sub
 
-    ' Parses the current line to check if it can run any actions on it.
+    ' This function is probably the hardest part of making MAWS...
+    ' Parses the current line to check if it has any requests on it.
     Private Function GetActionFromLine(ByVal Line As String)
-        Dim ActionRegex As New Regex("[A-Za-z0-9]+\.[A-Za-z0-9]+")
+        ' Regexes for determining categories of requests and if the specific line is a request.
+
+        ' Regex for determining if the line is a request. (Example: Merlin.Speak)
+        Dim RequestRegex As New Regex("[A-Za-z0-9]+\.[A-Za-z0-9]+")
+        ' Regex for determining if the line contains quotes. (Examples: "Welcome to the Microsoft Agent Web Supporter!", or "Greet")
         Dim QuotesRegex As New Regex("(("")?[A-Za-z0-9]+\(\)\s+&\s+)?""(?:[^""]|"""")*""(\s+&\s+[A-Za-z0-9]+\(\)""?(\s+&\s+)?)?")
+        ' Regex for determining if the line is a point on the screen (Examples: MerlinRightX MerlinBottomY, or 300, 240")
         Dim PointRegex As New Regex("[a-zA-Z0-9]+, [a-zA-Z0-9]+")
+        ' Regex for determining if the line is a single integer (Example: 128")
         Dim IntRegex As New Regex("[0-9]+")
 
-        If ActionRegex.IsMatch(Line) Then
-            Dim ActionMatch = ActionRegex.Match(Line).ToString.ToLower
-            Dim CharID = ActionMatch.Remove(ActionMatch.IndexOf("."))
+        ' Determines if the line is a request.
+        If RequestRegex.IsMatch(Line) Then
+            Dim RequestMatch = RequestRegex.Match(Line).ToString.ToLower
+            ' Gets the ID of the character preforming the request.
+            Dim CharID = RequestMatch.Remove(RequestMatch.IndexOf("."))
 
             If QuotesRegex.IsMatch(Line) Then
-                Dim FunctionRegex As New Regex("(""\s+&\s+)?[A-Za-z0-9]+\(\)(\s+&\s+"")?")
+                ' Regex to determine if the current line contains one or more time function. (Example: GetTimeOfDay())
+                Dim TimeRegex As New Regex("(""\s+&\s+)?[A-Za-z0-9]+\(\)(\s+&\s+"")?")
                 Dim QuotesMatch = QuotesRegex.Match(Line).ToString.Replace("""", "")
+                ' The string that the agent character speaks/thinks in the script.
                 Dim SpeakString As String
 
                 For Each M In QuotesRegex.Matches(Line)
                     SpeakString = SpeakString & M.ToString
                 Next
 
-                For Each M In FunctionRegex.Matches(SpeakString)
+                ' Replaces time functions with the current date and time.
+                For Each M In TimeRegex.Matches(SpeakString)
                     Dim Match = M.ToString.ToLower
 
                     If Match.Contains("gettimeofday") Then
@@ -134,22 +167,27 @@ Public Class AgentForm
                     End If
                 Next
 
+                ' Removes double quotes and the quotes at the beginning and end of the phrase.
                 SpeakString = SpeakString.Substring(1, SpeakString.Length - 2).Replace("""""", """")
 
-                If ActionMatch.Contains(".ttsmodeid") Then
+                If RequestMatch.Contains(".ttsmodeid") Then
+                    ' Sets the TTS Voice of the character if it's a TTSModeID Declaration
                     ControlAxAgent.Characters(CharID).TTSModeID = QuotesMatch
                     Return Nothing
-                ElseIf ActionMatch.Contains(".play") Then
+                ElseIf RequestMatch.Contains(".play") Then
+                    ' Makes the character play an animation.
                     ControlAxAgent.Characters(CharID).Get("Animation", QuotesMatch)
                     Return ControlAxAgent.Characters(CharID).Play(QuotesMatch)
-                ElseIf ActionMatch.Contains(".speak") Then
+                ElseIf RequestMatch.Contains(".speak") Then
+                    ' Makes the character speak the current speakstring.
                     If Not SpeakString = Nothing Then
                         Return ControlAxAgent.Characters(CharID).Speak(SpeakString)
                     End If
                     Return Nothing
-                ElseIf ActionMatch.Contains(".think") Then
+                ElseIf RequestMatch.Contains(".think") Then
+                    ' Makes the character think the current speakstring.
                     If Not SpeakString = Nothing Then
-                        Return ControlAxAgent.Characters(CharID).Speak(SpeakString)
+                        Return ControlAxAgent.Characters(CharID).Think(SpeakString)
                     End If
                     Return Nothing
                 End If
@@ -157,16 +195,24 @@ Public Class AgentForm
                 Return Nothing
             ElseIf PointRegex.IsMatch(Line) Then
                 Dim PointMatch As String = PointRegex.Match(Line).ToString
+                ' Screen Locations
                 Dim ScreenBottom As Integer = Screen.PrimaryScreen.Bounds.Bottom - ControlAxAgent.Characters(CharID).Height
                 Dim ScreenLeft As Integer = Screen.PrimaryScreen.Bounds.Left
                 Dim ScreenRight As Integer = Screen.PrimaryScreen.Bounds.Right - ControlAxAgent.Characters(CharID).Width
                 Dim ScreenTop As Integer = Screen.PrimaryScreen.Bounds.Top
+                ' Get's the X and Y location that the character is supposed to move to or gesture at. (Can be a string replacement like BottomX, or an integer like 300)
                 Dim LocationX As String = PointMatch.Remove(PointMatch.IndexOf(","))
                 Dim LocationY As String = PointMatch.Substring(PointMatch.IndexOf(",") + 1)
+                ' Actual X and Y position on screen that the character is meant to move to or gesture at.
                 Dim PointX As Integer
                 Dim PointY As Integer
 
-                If LocationX.ToLower.Contains("left") Then
+                ' Replacement values for X.
+                If LocationX.ToLower.Contains("leftcenter") Then
+                    PointX = ScreenRight / 4
+                ElseIf LocationX.ToLower.Contains("rightcenter") Then
+                    PointX = (ScreenRight / 4) * 3
+                ElseIf LocationX.ToLower.Contains("left") Then
                     PointX = ScreenLeft
                 ElseIf LocationX.ToLower.Contains("center") Then
                     PointX = ScreenRight / 2
@@ -178,7 +224,12 @@ Public Class AgentForm
                     Return 0
                 End If
 
-                If LocationY.ToLower.Contains("top") Then
+                ' Replacement values for Y.
+                If LocationY.ToLower.Contains("topcenter") Then
+                    PointX = ScreenBottom / 4
+                ElseIf LocationY.ToLower.Contains("bottomcenter") Then
+                    PointX = (ScreenBottom / 4) * 3
+                ElseIf LocationY.ToLower.Contains("top") Then
                     PointY = ScreenTop
                 ElseIf LocationY.ToLower.Contains("center") Then
                     PointY = ScreenBottom / 2
@@ -190,9 +241,10 @@ Public Class AgentForm
                     Return 0
                 End If
 
-                If ActionMatch.Contains(".moveto") Then
+                ' Makes the character move to or gesture at a point on the screen.
+                If RequestMatch.Contains(".moveto") Then
                     Return ControlAxAgent.Characters(CharID).MoveTo(PointX, PointY)
-                ElseIf ActionMatch.Contains(".gestureat") Then
+                ElseIf RequestMatch.Contains(".gestureat") Then
                     Return ControlAxAgent.Characters(CharID).GestureAt(PointX, PointY)
                 End If
 
@@ -200,25 +252,35 @@ Public Class AgentForm
             ElseIf IntRegex.IsMatch(Line) Then
                 Dim IntMatch As Integer = Convert.ToInt16(IntRegex.Match(Line).ToString)
 
-                If ActionMatch.Contains(".width") Then
+                ' Sets the width height of the character depending on the declaration.
+                If RequestMatch.Contains(".width") Then
                     ControlAxAgent.Characters(CharID).Width = IntMatch
-                ElseIf ActionMatch.Contains(".height") Then
+                ElseIf RequestMatch.Contains(".height") Then
                     ControlAxAgent.Characters(CharID).Height = IntMatch
                 End If
 
                 Return Nothing
-            ElseIf ActionMatch.Contains(".show") Then
+            ElseIf RequestMatch.Contains(".show") Then
+                ' Shows the character if it's a show request.
                 Return ControlAxAgent.Characters(CharID).Show()
-            ElseIf ActionMatch.Contains(".hide") Then
+            ElseIf RequestMatch.Contains(".hide") Then
+                ' Hides the character if it's a show request. (Not to be confused with HideReq)
                 Return ControlAxAgent.Characters(CharID).Hide()
-            Else
-                Return Nothing
+            ElseIf RequestMatch.Contains(".stop") Then
+                ' Stops the current requests of the character if it's a stop request.
+                ControlAxAgent.Characters(CharID).Stop()
+            ElseIf RequestMatch.Contains(".stopall") Then
+                ' Stops every request of the character if it's a stopall request.
+                ControlAxAgent.Characters(CharID).StopAll()
             End If
+
+            Return Nothing
         Else
             Return Nothing
         End If
     End Function
 
+    ' Gets the time of day. (For Example: Evening)
     Private Function GetTimeOfDay() As String
         If Date.Now.Hour < 12 Then
             Return "Morning"
@@ -229,22 +291,38 @@ Public Class AgentForm
         End If
     End Function
 
+    ' Gets the current Time. (For Example: 12:00 PM)
     Private Function GetTime() As String
         Return Date.Now.ToShortTimeString
     End Function
 
-    ' Gets the name of the current Day of the Week.
+    ' Gets the name of the current Day of the Week. (For Example: Tuesday)
     Private Function GetDay() As String
         Dim DayName As String() = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
 
         Return DayName(Date.Now.DayOfWeek)
     End Function
 
+    ' Gets the current Date. (For Example: October 28th, 2025)
     Private Function GetDate() As String
-        Return MonthName(Date.Now.Month) & " " & Date.Now.Day & ", " & Date.Now.Year
+        Dim DayStr As String
+        Dim DayInt = Date.Now.Day
+
+        Select Case DayInt
+            Case 1
+                DayStr = DayInt & "st"
+            Case 2
+                DayStr = DayInt & "nd"
+            Case 3
+                DayStr = DayInt & "rd"
+            Case Else
+                DayStr = DayInt & "th"
+        End Select
+
+        Return MonthName(Date.Now.Month) & " " & DayStr & ", " & Date.Now.Year
     End Function
 
-    ' Checks if the current date is the holiday.
+    ' Checks if the current date is the holiday and gets the holiday. (For Example: Christmas)
     Private Function Holiday() As String
         If Date.Now.Month = 1 And Date.Now.Day = 1 Then
             Return "Happy New Year!"
@@ -329,11 +407,13 @@ Public Class AgentForm
         End If
     End Sub
 
+    ' Hides every character visible.
     Private Sub HideAllCharacters()
         For i = 0 To CharIDs.Count - 1
             Dim CharID = CharIDs(i)
             Dim AgentChar = ControlAxAgent.Characters(CharID)
 
+            ' Checks if the character is visible or has other clients active.
             If Not AgentChar.HasOtherClients And AgentChar.Active Then
                 HideReq = AgentChar.Hide
                 WaitFor(HideReq)
@@ -343,19 +423,53 @@ Public Class AgentForm
 
     Private Sub ControlAxAgent_RequestComplete(ByVal sender As System.Object, ByVal e As AxAgentObjects._AgentEvents_RequestCompleteEvent) Handles ControlAxAgent.RequestComplete
         If e.request Is HideReq Then
+            ' The amount of characters unloaded in the script.
             Dim UnloadedChars = 0
 
             For Each CharID In CharIDs
                 Dim AgentChar = ControlAxAgent.Characters(CharID)
 
+                ' Checks to see if each character is visible before the app closes. If they're not, unloads them and increments the UnloadedChars by 1.
                 If AgentChar.Visible = False Then
+                    ControlAxAgent.Characters.Unload(CharID)
                     UnloadedChars = UnloadedChars + 1
                 End If
 
+                ' Closes the app if the amount of characters hidden is equal to the amount of character IDs in the script.
                 If UnloadedChars = CharIDs.Count Then
                     Application.Exit()
                 End If
             Next
         End If
+    End Sub
+
+    ' Preforms an action if the agent script contains a command.
+    Private Sub ControlAxAgent_Command(ByVal sender As System.Object, ByVal e As AxAgentObjects._AgentEvents_CommandEvent) Handles ControlAxAgent.Command
+        Dim CommandName As String = e.userInput.name
+        Dim CurrentParse As String
+
+        For Each RL In ScriptText.Split(Chr(10))
+            ' Removes unnecessary characters from the line like spaces that come after the line.
+            Dim Line = RL.Trim
+
+            If Not Line = String.Empty Then
+                ' Checks if the line is a list declaration, if it is, set it as the current parse.
+                ' If the line isn't declaring a list, it checks to make sure the current parse is a commandscript for the selected command.
+                If Line.StartsWith("[") Then
+                    CurrentParse = Line
+                ElseIf CurrentParse = "[CommandScript:" & CommandName & "]" Then
+                    If Line.ToLower.Contains("set req =") Then
+                        ' Sets the current request to the action listed after the equal sign.
+                        Req = GetActionFromLine(Line)
+                    ElseIf Line.ToLower = "waitfor req" Then
+                        ' Has the program wait for a specific request before running the next line.
+                        WaitFor(Req)
+                    Else
+                        ' Causes a specific character to do an action from the line.
+                        GetActionFromLine(Line)
+                    End If
+                End If
+            End If
+        Next
     End Sub
 End Class
